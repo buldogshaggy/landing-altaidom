ad359821d8e1e0387234ab11745cd5af4d49b9cc
buldogshaggy <buldo9shaggy@yandex.ru>
2026-03-27 14:32:14 +0700

    setup static export

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,18 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+
+  output: "export",
+
+  images: {
+    unoptimized: true,
+  },
+
+  basePath: isProd ? "/landing-altaidom" : "",
+  assetPrefix: isProd ? "/landing-altaidom/" : "",
 };
 
 export default nextConfig;
